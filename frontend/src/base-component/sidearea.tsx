b742d88d5a8b3a6b32ec41e5e897fcf77d334e23
grkon <yo.for.user@gmail.com>
2025-07-24 02:01:40 +0900

    impl styles on sidearea

--- frontend/src/base-component/sidearea.tsx
+++ frontend/src/base-component/sidearea.tsx
@@ -9,7 +9,7 @@ type Props = {
 const DefaultItems = (): React.JSX.Element[] => {
   var items: React.JSX.Element[] = [];
   items.push(
-    <div className={styles.sa_item}>
+    <div className={`${styles.sa_item} ${styles.sa_search}`} key="sai_search">
       <div className={styles.sai_title}>検索窓</div>
       <div className={styles.sai_content}>
         <input className="sac_search_input" />
