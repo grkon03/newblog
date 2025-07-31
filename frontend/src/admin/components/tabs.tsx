c0c0b300440d1255fa2016994646f96a0d64a6b9
grkon <yo.for.user@gmail.com>
2025-07-31 11:18:10 +0900

    reafctoring

---
+++ frontend/src/admin/components/tabs.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import MDEditor from './mdeditor';
+import MDPreview from './mdpreview';
+import styles from './tabs.module.css';
+
+type SelectedTab = 'editor' | 'preview';
+
+type Props = {
+  setMDtext: (text: string) => void;
+  setUploadedImages: (images: Map<string, File>) => void;
+};
+
+const Tabs: React.FC<Props> = ({ setMDtext, setUploadedImages }) => {
+  const [MDtext, _setMDtext] = useState('');
+  const [uploadedImages, _setUploadedImages] = useState<Map<string, File>>(
+    new Map<string, File>()
+  );
+  const [selectedTab, setSelectedTab] = useState<SelectedTab>('editor');
+  const handleClickTabSwitch = (s: SelectedTab) => () => {
+    setSelectedTab(s);
+  };
+  const isSelected = (s: SelectedTab) => s === selectedTab;
+
+  useEffect(() => {
+    setMDtext(MDtext);
+  }, [setMDtext, MDtext]);
+  useEffect(() => {
+    setUploadedImages(uploadedImages);
+  }, [setUploadedImages, uploadedImages]);
+
+  return (
+    <div>
+      <div className={styles.tabswitch}>
+        <div
+          className={`${styles.switchbutton} ${
+            isSelected('editor') ? styles.selected : ''
+          }`}
+          onClick={handleClickTabSwitch('editor')}
+        >
+          編集
+        </div>
+        <div
+          className={`${styles.switchbutton} ${
+            isSelected('preview') ? styles.selected : ''
+          }`}
+          onClick={handleClickTabSwitch('preview')}
+        >
+          プレビュー
+        </div>
+      </div>
+      <div>
+        <div className={`${styles.editor} ${styles['s__' + selectedTab]}`}>
+          <MDEditor setText={_setMDtext} setImages={_setUploadedImages} />
+        </div>
+        <div className={`${styles.preview} ${styles['s__' + selectedTab]}`}>
+          <MDPreview MDtext={MDtext} images={uploadedImages} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
--- frontend/src/admin/createarticle.tsx
+++ frontend/src/admin/createarticle.tsx
@@ -1,61 +1,26 @@
 import React, { useState } from 'react';
-import MDEditor from './components/mdeditor';
-import MDPreview from './components/mdpreview';
+import Tabs from './components/tabs';
 import SubmitButtons from './components/submitbuttons';
 import { MainAreaProps, InitSideArea } from '../types';
 import { AdminSA } from '../base-component/sidearea/admin';
-import styles from './createarticle.module.css';
 
 type Props = {
   mainareaprops?: MainAreaProps;
 };
 
-type SelectedTab = 'editor' | 'preview';
-
 const CreateArticle: React.FC<Props> = ({ mainareaprops }) => {
   InitSideArea(mainareaprops, AdminSA);
-  const [MDtext, setMDtext] = useState('');
-  const [uploadedImages, setUploadedImages] = useState<Map<string, File>>(
+  const [, setMDtext] = useState('');
+  const [, setUploadedImages] = useState<Map<string, File>>(
     new Map<string, File>()
   );
-  const [selectedTab, setSelectedTab] = useState<SelectedTab>('editor');
-  const handleClickTabSwitch = (s: SelectedTab) => () => {
-    setSelectedTab(s);
-  };
-  const isSelected = (s: SelectedTab) => s === selectedTab;
 
   const handleSaveClick = () => {};
   const handlePublishClick = () => {};
 
   return (
     <div>
-      <h2>記事を作成する</h2>
-      <div className={styles.tabswitch}>
-        <div
-          className={`${styles.switchbutton} ${
-            isSelected('editor') ? styles.selected : ''
-          }`}
-          onClick={handleClickTabSwitch('editor')}
-        >
-          編集
-        </div>
-        <div
-          className={`${styles.switchbutton} ${
-            isSelected('preview') ? styles.selected : ''
-          }`}
-          onClick={handleClickTabSwitch('preview')}
-        >
-          プレビュー
-        </div>
-      </div>
-      <div>
-        <div className={`${styles.editor} ${styles['s__' + selectedTab]}`}>
-          <MDEditor setText={setMDtext} setImages={setUploadedImages} />
-        </div>
-        <div className={`${styles.preview} ${styles['s__' + selectedTab]}`}>
-          <MDPreview MDtext={MDtext} images={uploadedImages} />
-        </div>
-      </div>
+      <Tabs setMDtext={setMDtext} setUploadedImages={setUploadedImages} />
       <SubmitButtons
         onSaveClick={handleSaveClick}
         onPublishClick={handlePublishClick}
